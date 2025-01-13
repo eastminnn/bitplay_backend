10f05e10d2a4687af940a21443ffee503941a82d
eastminnn <oh1334000@gmail.com>
2025-01-13 21:56:34 +0900

    Update user entity file

--- src/main/java/com/springboot/basic/auth/entity/User.java
+++ src/main/java/com/springboot/basic/auth/entity/User.java
@@ -2,6 +2,10 @@ package com.springboot.basic.auth.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.stereotype.Controller;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -11,6 +15,25 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // DB에서 자동 생성
     private Long id;
 
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
 
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String useremail;
+
+    @CreationTimestamp // 생성시간 자동기록
+    @Column(updatable = false) // 생성시간 수정 불가
+    private LocalDateTime created;
+
+    @Enumerated(EnumType.STRING) // 사용자 관리 (열거형)
+    @Column(nullable = false)
+    private Role role; // 사용자 권한
+
+    public enum Role {
+        USER, ADMIN
+    }
 
 }
