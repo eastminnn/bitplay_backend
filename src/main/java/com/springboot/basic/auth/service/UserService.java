2af0e7aadf220df541faa45f6e23821eece256ac
eastminnn <oh1334000@gmail.com>
2025-01-16 06:39:25 +0900

    [수정] UserServiceImpl file

--- src/main/java/com/springboot/basic/auth/service/UserService.java
+++ src/main/java/com/springboot/basic/auth/service/UserService.java
@@ -5,7 +5,10 @@ import com.springboot.basic.auth.entity.User;
 import java.util.Optional;
 
 public interface UserService {
+    // 사용자 등록
     User registerUser(User user);
+
     Optional<User> findUserByUsername(String username);
+
     Optional<User> findUserByUseremail(String useremail);
 }
--- src/main/java/com/springboot/basic/auth/service/UserServiceImpl.java
+++ src/main/java/com/springboot/basic/auth/service/UserServiceImpl.java
@@ -1,26 +1,32 @@
 package com.springboot.basic.auth.service;
 
 import com.springboot.basic.auth.entity.User;
+import com.springboot.basic.auth.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     // 사용자 등록
     @Override
     public User registerUser(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public Optional<User> findUserByUsername(String username) {
-        return Optional.empty();
+        return userRepository.findByUsername(username);
     }
 
     @Override
     public Optional<User> findUserByUseremail(String useremail) {
-        return Optional.empty();
+        return userRepository.findByUseremail(useremail);
     }
 }
