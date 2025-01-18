4b32c519c871249f07190cf2fd954969fcad2e9f
eastminnn <oh1334000@gmail.com>
2025-01-18 20:29:14 +0900

    [수정] UserController file

--- src/main/java/com/springboot/basic/auth/controller/UserController.java
+++ src/main/java/com/springboot/basic/auth/controller/UserController.java
@@ -7,6 +7,7 @@ import com.springboot.basic.auth.dto.RegisterResponseDto;
 import com.springboot.basic.auth.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -33,4 +34,5 @@ public class UserController {
         LoginResponseDto loginResponseDto = userService.login(loginRequestDto);
         return ResponseEntity.ok(loginResponseDto);
     }
+
 }
