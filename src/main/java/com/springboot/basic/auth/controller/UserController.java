e5356ccb5741019230d6b7c5622a9cf0bd9b5d99
eastminnn <oh1334000@gmail.com>
2025-01-18 18:35:00 +0900

    [추가] UserController file

---
+++ src/main/java/com/springboot/basic/auth/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.springboot.basic.auth.controller;
+
+import com.springboot.basic.auth.dto.LoginRequestDto;
+import com.springboot.basic.auth.dto.LoginResponseDto;
+import com.springboot.basic.auth.dto.RegisterRequestDto;
+import com.springboot.basic.auth.dto.RegisterResponseDto;
+import com.springboot.basic.auth.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users") // API 기본 URL 경로 설정
+public class UserController {
+
+    private final UserService userService;
+
+    // 생성자 주입
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Register
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponseDto> register(@RequestBody RegisterRequestDto registerRequestDto) {
+        RegisterResponseDto responseDto = userService.register(registerRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    // login
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        LoginResponseDto loginResponseDto = userService.login(loginRequestDto);
+        return ResponseEntity.ok(loginResponseDto);
+    }
+}
