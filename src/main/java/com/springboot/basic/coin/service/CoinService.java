6edf23ecfee0f10176ed28771dffc7e8141453be
eastminnn <oh1334000@gmail.com>
2025-01-21 18:48:16 +0900

    [추가] CoinService

--- src/main/java/com/springboot/basic/coin/service/CoinService.java
+++ src/main/java/com/springboot/basic/coin/service/CoinService.java
@@ -1,2 +1,25 @@
-package com.springboot.basic.coin.service;public interface CoinService {
+package com.springboot.basic.coin.service;
+
+import com.springboot.basic.coin.dto.CoinRequestDto;
+import com.springboot.basic.coin.dto.CoinResponseDto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface CoinService {
+    // 코인 등록
+    CoinResponseDto addCoin(CoinRequestDto coinRequestDto);
+
+    // 코인 삭제
+    CoinResponseDto deleteCoin(CoinResponseDto coinResponseDto);
+
+    // 전체 코인 목록 조회
+    List<CoinResponseDto> getAllcoins();
+
+    // 특정 코인 조회
+    CoinResponseDto getCoinBySymbol(String symbol);
+
+    // 코인 가격 갱신
+    void updateCoinPrices(String symbol, BigDecimal newprice);
+
 }
--- src/main/java/com/springboot/basic/coin/service/CoinServiceImpl.java
+++ src/main/java/com/springboot/basic/coin/service/CoinServiceImpl.java
@@ -1,2 +1,56 @@
-package com.springboot.basic.coin.service;public class CoinServiceImpl {
+package com.springboot.basic.coin.service;
+
+import com.springboot.basic.coin.dto.CoinRequestDto;
+import com.springboot.basic.coin.dto.CoinResponseDto;
+import com.springboot.basic.coin.entity.Coin;
+import com.springboot.basic.coin.repository.CoinRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class CoinServiceImpl implements CoinService{
+    private final CoinRepository coinRepository;
+
+    public CoinServiceImpl(CoinRepository coinRepository) {
+        this.coinRepository = coinRepository;
+    }
+
+    // 코인 추가
+    @Override
+    public CoinResponseDto addCoin(CoinRequestDto coinRequestDto) {
+        if (coinRepository.findBySymbol(coinRequestDto.getSymbol()).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 코인입니다.");
+        }
+        Coin coin = new Coin();
+        coin.setSymbol(coinRequestDto.getSymbol());
+        coin.setName(coinRequestDto.getName());
+        coin.setPrice(BigDecimal.ZERO); // 기본값 = 0
+        coin.setUpdated(LocalDateTime.now());
+
+
+    }
+
+
+    @Override
+    public CoinResponseDto deleteCoin(CoinResponseDto coinResponseDto) {
+        return null;
+    }
+
+    @Override
+    public List<CoinResponseDto> getAllcoins() {
+        return List.of();
+    }
+
+    @Override
+    public CoinResponseDto getCoinBySymbol(String symbol) {
+        return null;
+    }
+
+    @Override
+    public void updateCoinPrices(String symbol, BigDecimal newprice) {
+
+    }
 }
