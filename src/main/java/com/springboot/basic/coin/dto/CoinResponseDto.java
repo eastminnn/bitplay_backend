cf3b359860c7b1ec0f8198cc85be038cd5288477
eastminnn <oh1334000@gmail.com>
2025-01-21 18:47:05 +0900

    [추가] CoinResponseDto

--- src/main/java/com/springboot/basic/coin/dto/CoinResponseDto.java
+++ src/main/java/com/springboot/basic/coin/dto/CoinResponseDto.java
@@ -1,4 +1,23 @@
 package com.springboot.basic.coin.dto;
 
-public class CoinResposeDto {
+import com.springboot.basic.coin.entity.Coin;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CoinResponseDto {
+    private String symbol;
+    private String name;
+    private BigDecimal price;
+    private String updated;
+
+    private CoinResponseDto coverToResponseDto(Coin coin) {
+        CoinResponseDto dto = new CoinResponseDto();
+        dto.setSymbol(coin.getSymbol());
+        dto.setName(coin.getName());
+        dto.setPrice(coin.getPrice());
+        dto.setUpdated(coin.getUpdated().toString());
+        return dto;
+    }
 }
