1ba51f69ff1d7aeb39f04f2750bdb54d5cd68935
eastminnn <oh1334000@gmail.com>
2025-01-21 20:50:25 +0900

    [수정] CoinResponseDto.java

--- src/main/java/com/springboot/basic/coin/dto/CoinResponseDto.java
+++ src/main/java/com/springboot/basic/coin/dto/CoinResponseDto.java
@@ -1,6 +1,6 @@
 package com.springboot.basic.coin.dto;
 
-import com.springboot.basic.coin.entity.Coin;
+
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -11,13 +11,4 @@ public class CoinResponseDto {
     private String name;
     private BigDecimal price;
     private String updated;
-
-    private CoinResponseDto coverToResponseDto(Coin coin) {
-        CoinResponseDto dto = new CoinResponseDto();
-        dto.setSymbol(coin.getSymbol());
-        dto.setName(coin.getName());
-        dto.setPrice(coin.getPrice());
-        dto.setUpdated(coin.getUpdated().toString());
-        return dto;
-    }
 }
